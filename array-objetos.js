ff81b444408a868121aee96367d7d29a44f2e595
Aline <Aline.soares.2@hotmail.com>
2019-10-09 17:20:05 -0300

    adicionar novos exercicios de array

--- array-objetos.js
+++ array-objetos.js
@@ -37,6 +37,26 @@ console.log(comprimentoMedioPalavras("xuxu","beleza"));
 // Exemplo de entrada:
 // console.log(pegaElemento([1, 3, 5], 1);
 
-function pegaElemento(valor){
-
-}
+function pegaElemento(arr, num){
+    //     for (let i =0; i<arr.length; i++ ){
+    //        return arr[num];
+        
+    // }
+    // console.log(pegaElemento([1, 3, 5, 7, 3, 9], 5));
+    
+    // Escreva uma função chamada credencialEhValida.
+    // Dado um nome e uma senha, credencialEhValida retorna true se o nome contiver mais do que
+    // 3 caracteres e a senha, pelo menos 8 caracteres. Caso contrário, retorna false.
+    
+    // Exemplo:
+    // console.log(credencialEhValida('Xuxu', 'minhasenha'));
+    // function credencialEhValida(nome, senha){
+    //     if (nome.length >=3 && senha.length>=8){
+    //         return true
+        
+    //     }else {
+    //         return false
+    //     }
+        
+    // }
+    // console.log(credencialEhValida('abacaxi', 'minhasenha'));
